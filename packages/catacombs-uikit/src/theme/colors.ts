a0af153a81f7ae505ac3120721bd7d8117b75961
Nnamdi Arimah <nnamdi.arimah@shopify.com>
2021-11-05 21:28:06 -0400

    initial commit

---
+++ packages/catacombs-uikit/src/theme/colors.ts
@@ -0,0 +1,67 @@
+import { Colors } from "./types";
+
+export const baseColors = {
+  failure: "#ED4B9E",
+  primary: "#007737",
+  primaryBright: "#53DEE9",
+  primaryDark: "#0098A1",
+  secondary: "#b4bd00",
+  success: "#31D0AA",
+  warning: "#FFB237",
+};
+
+export const brandColors = {
+  binance: "#F0B90B",
+};
+
+export const lightColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  background: "#000000",
+  backgroundDisabled: "#ffffff",
+  backgroundAlt: "#000000",         // Side menu
+  contrast: "#191326",
+  dropdown: "#ffffff",
+  invertedContrast: "#ffffff",
+  input: "#ffffff",
+  inputSecondary: "#d7caec",
+  tertiary: "#ffffff",
+  text: "#ffffff",
+  textDisabled: "#BDC2C4",
+  textSubtle: "#F1F2F1",
+  borderColor: "#ffffff",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #E6FDFF 0%, #F3EFFF 100%)",
+    cardHeader: "linear-gradient(111.68deg, #F2ECF2 0%, #E8F2F6 100%)",
+    blue: "linear-gradient(180deg, #A7E8F1 0%, #94E1F2 100%)",
+    violet: "linear-gradient(180deg, #E2C9FB 0%, #CDB8FA 100%)",
+    violetAlt: "linear-gradient(180deg, #CBD7EF 0%, #9A9FD0 100%)",
+  },
+};
+
+export const darkColors: Colors = {
+  ...baseColors,
+  ...brandColors,
+  secondary: "#9A6AFF",
+  background: "#000000",
+  backgroundDisabled: "#3c3742",
+  backgroundAlt: "#000000",
+  contrast: "#FFFFFF",
+  dropdown: "#1E1D20",
+  invertedContrast: "#191326",
+  input: "#483f5a",
+  inputSecondary: "#66578D",
+  primaryDark: "#0098A1",
+  tertiary: "#353547",
+  text: "#EAE2FC",
+  textDisabled: "#666171",
+  textSubtle: "#A28BD4",
+  borderColor: "#524B63",
+  gradients: {
+    bubblegum: "linear-gradient(139.73deg, #313D5C 0%, #3D2A54 100%)",
+    cardHeader: "linear-gradient(166.77deg, #3B4155 0%, #3A3045 100%)",
+    blue: "linear-gradient(180deg, #00707F 0%, #19778C 100%)",
+    violet: "linear-gradient(180deg, #6C4999 0%, #6D4DB2 100%)",
+    violetAlt: "linear-gradient(180deg, #434575 0%, #66578D 100%)",
+  },
+};
---
+++ packages/catacombs-uikit/src/widgets/Menu/theme.ts
@@ -0,0 +1,11 @@
+import { NavTheme } from "./types";
+
+export const light: NavTheme = {
+  background: "#000000",
+  hover: "#EEEAF4",
+};
+
+export const dark: NavTheme = {
+  background: "#000000",
+  hover: "#473d5d",
+};
